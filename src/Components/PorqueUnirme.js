f57f1df1640d7b78d4184e2bb21bc99842c138c2
Mateo Flores <dragonnegro1231@gmail.com>
2021-12-06 11:52:05 -0500

    Primera version multi plataforma a espera de correcciones(La version no convence al programador)

--- src/Components/PorqueUnirme.js
+++ src/Components/PorqueUnirme.js
@@ -7,21 +7,24 @@ function PorqueUnirme(){
     return(
         <section id="porqueUnirme">
             <h2> </h2>
-            <div className="porqueUnirme--tarjetas-info">
-                <img src={imgCampamento} alt="campamentos con charlas de codigo"/>
-                <h3>Campamentos con charlas de código</h3>
-                <p>Se realizan por paseos por fin de ciclo con el fin de socializar ideas y compartir experiencias. </p>
+            <div className="porqueUnirme--tarjetas--container">
+                <div className="porqueUnirme--tarjetas-info">
+                    <img src={imgCampamento} alt="campamentos con charlas de codigo"/>
+                    <h3>Campamentos con charlas de código</h3>
+                    <p>Se realizan por paseos por fin de ciclo con el fin de socializar ideas y compartir experiencias. </p>
+                </div>
+                <div className="porqueUnirme--tarjetas-info-reverse">
+                    <img src={impTutor_Estudiante} alt="campamentos con charlas de codigo"/>
+                    <h3>Tutor-estudiante</h3>
+                    <p>Cooperación entre estudiantes de ciclos mayores con estudiantes de ciclos menos.</p>
+                </div>
+                <div className="porqueUnirme--tarjetas-info">
+                    <img src={imgEntrenamiento} alt="campamentos con charlas de codigo"/>
+                    <h3>Entrenamiento</h3>
+                    <p>Cada cierto tiempo se dara clases de programacion y posterior se asignará un reto a un grupo, para después socializar la solución del ejercicio.</p>
+                </div>
             </div>
-            <div className="porqueUnirme--tarjetas-info-reverse">
-                <img src={impTutor_Estudiante} alt="campamentos con charlas de codigo"/>
-                <h3>Tutor-estudiante</h3>
-                <p>Cooperación entre estudiantes de ciclos mayores con estudiantes de ciclos menos.</p>
-            </div>
-            <div className="porqueUnirme--tarjetas-info">
-                <img src={imgEntrenamiento} alt="campamentos con charlas de codigo"/>
-                <h3>Entrenamiento</h3>
-                <p>Cada cierto tiempo se dara clases de programacion y posterior se asignará un reto a un grupo, para después socializar la solución del ejercicio.</p>
-            </div>
+            
         </section>
     );
 }
